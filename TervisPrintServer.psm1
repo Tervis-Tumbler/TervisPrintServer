08e53dac55669dbc7c6be7ed09fa523db2ae3276
Greg <gkjono@tervis.com>
2017-04-13 15:51:26 -0400

    Added a few drivers
    Discontinuing work for a while as priorities have changed.

--- TervisPrintServer.psm1
+++ TervisPrintServer.psm1
@@ -13,11 +13,32 @@ function Install-PrintServerDriversFromWindowsUpdate {
         [Parameter(ValueFromPipelineByPropertyName)]$ComputerName
     )
     Invoke-Command -ScriptBlock {
-        $ZebraDriver = get-windowsdriver -Online | 
+        $BrotherHL6180DWDriver = Get-WindowsDriver -Online | 
+        where providername -eq Brother |
+        where ClassName -eq Printer |
+        where Version -eq 1.5.0.0
+        if (-not $BrotherHL6180DWDriver) {
+            Invoke-WebRequest http://download.windowsupdate.com/msdownload/update/driver/drvs/2012/09/20501715_fe2de9f3194ab758a30d4736e7bfcee408f187dd.cab -OutFile $env:TEMP\BrotherHL6180DWDriver.cab
+            New-Item -Path $env:TEMP\BrotherHL6180DWDriver -ItemType Directory
+            expand.exe -F:* $env:TEMP\BrotherHL6180DWDriver.cab $env:TEMP\BrotherHL6180DWDriver
+            pnputil.exe -i -a $env:TEMP\BrotherHL6180DWDriver\BROHLB1A.INF
+        }
+
+        $KyoceraUniversalDriver = Get-WindowsDriver -Online | 
+        where providername -eq Kyocera |
+        where ClassName -eq Printer |
+        where Version -eq 6.0.2726.0
+        if (-not $KyoceraUniversalDriver) {
+            Invoke-WebRequest http://download.windowsupdate.com/d/msdownload/update/driver/drvs/2013/11/20557306_dad42303dba3df7ee060e26fffd35cd483bf7844.cab -OutFile $env:TEMP\KyoceraUniversalDriver.cab
+            New-Item -Path $env:TEMP\KyoceraUniversalDriver -ItemType Directory
+            expand.exe -F:* $env:TEMP\KyoceraUniversalDriver.cab $env:TEMP\KyoceraUniversalDriver
+            pnputil.exe -i -a $env:TEMP\KyoceraUniversalDriver\OEMSETUP.INF
+        }
+
+        $ZebraDriver = Get-WindowsDriver -Online | 
         where providername -eq zebra |
         where ClassName -eq Printer |
         where Version -eq 5.1.7.6290
-
         if (-not $ZebraDriver) {
             Invoke-WebRequest http://download.windowsupdate.com/c/msdownload/update/driver/drvs/2016/06/20857735_abfb8f058ce8dd7bbb70ec4a7df3947f81b204a8.cab -OutFile $env:TEMP\ZDesigner.cab
             New-Item -Path $env:TEMP\zdesigner -ItemType Directory
